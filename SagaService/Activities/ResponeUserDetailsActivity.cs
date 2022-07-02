1b48e3d9b2a3420a888dcab0c7a15a74932c4ec5
Yehuda Kremer <yehudakremer@gmail.com>
2022-07-03 00:22:37 +0300

    custom activity and composite event

---
+++ SagaService/Activities/ResponeUserDetailsActivity.cs
@@ -0,0 +1,38 @@
+﻿using MassTransit;
+using ClassLibrary1.Events;
+
+namespace SagaService.Activities
+{
+    public class ResponeUserDetailsActivity : IStateMachineActivity<UserState, UserDetailsFound>
+    {
+        private readonly ConsumeContext context;
+
+        public ResponeUserDetailsActivity(ConsumeContext context)
+        {
+            this.context = context;
+        }
+
+        public void Accept(StateMachineVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public async Task Execute(BehaviorContext<UserState, UserDetailsFound> c, IBehavior<UserState, UserDetailsFound> next)
+        {
+            var endpoint = await c.GetSendEndpoint(c.Saga.ResponseAddress);
+            await endpoint.Send(new UserDetailsFound(c.Saga.CorrelationId, c.Message.User));
+
+            await next.Execute(c).ConfigureAwait(false);
+        }
+
+        public Task Faulted<TException>(BehaviorExceptionContext<UserState, UserDetailsFound, TException> context, IBehavior<UserState, UserDetailsFound> next) where TException : Exception
+        {
+            return next.Faulted(context);
+        }
+
+        public void Probe(ProbeContext context)
+        {
+            context.CreateScope(nameof(UserDetailsFound));
+        }
+    }
+}
--- SagaService/UserStateMachine.cs
+++ SagaService/UserStateMachine.cs
@@ -1,14 +1,17 @@
 ﻿using MassTransit;
 using ClassLibrary1.Commands;
 using ClassLibrary1.Events;
+using SagaService.Activities;
+using MassTransit.SagaStateMachine;
 
 namespace SagaService
 {
     public class UserState : SagaStateMachineInstance
     {
         public Guid CorrelationId { get; set; }
-        public int CurrentState { get; set; }
+        public string CurrentState { get; set; }
         public Uri ResponseAddress { get; set; }
+        public int MyEventStatus { get; set; }
     }
 
     public class UserStateMachine : MassTransitStateMachine<UserState>
@@ -18,31 +21,32 @@ namespace SagaService
 
         // Events/Commands used in this saga
         public Event<GetUserDetails> GetUserDetails { get; private set; }
-        public Event<UserDetailsFound> GotUserDetails { get; private set; }
+        public Event<UserDetailsFound> UserDetailsFound { get; private set; }
+        public Event MyEvent { get; private set; }
 
         public UserStateMachine()
         {
-            InstanceState(x => x.CurrentState, GettingUserDetails);
+            InstanceState(x => x.CurrentState);
 
             Initially(
                 When(GetUserDetails)
-                    .Then(c =>
-                    {
-                        c.Saga.ResponseAddress = c.ResponseAddress;
-                    })
+                    .Then(c => c.Saga.ResponseAddress = c.ResponseAddress)
                     .Publish(c => new FindUserDetailsByID(c.Saga.CorrelationId, c.Message.ID))
                     .TransitionTo(GettingUserDetails));
 
             During(GettingUserDetails,
-                When(GotUserDetails)
-                .ThenAsync(async c =>
-                {
-                    var endpoint = await c.GetSendEndpoint(c.Saga.ResponseAddress);
-                    await endpoint.Send(new UserDetailsFound(c.Saga.CorrelationId, c.Message.User));
-                })
-                .Finalize());
+                When(UserDetailsFound)
+                .Activity(x => x.OfType<ResponeUserDetailsActivity>()));
+
+            CompositeEvent(MyEvent, x => x.MyEventStatus, GetUserDetails, UserDetailsFound);
+
+            DuringAny(
+                When(MyEvent)
+                    .Then(context => Console.WriteLine("Got MyEvent, Current status is {0}", context.Saga.CurrentState)));
 
             SetCompletedWhenFinalized();
         }
     }
+
+
 }
